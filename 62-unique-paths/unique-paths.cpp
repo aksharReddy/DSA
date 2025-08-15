3de5ccf5ea2764094f4d00e2519a12b1c0617c6f
aksharReddy <127131253+aksharReddy@users.noreply.github.com>
2025-08-15 22:05:12 +0530

    Time: 2 ms (18.55%) | Memory: 9.3 MB (58.07%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
 
-    int noofpaths(int i,int j,int m,int n, vector<vector<int>>&dp){
-        if(i==m-1 && j ==n-1 ) return 1;
-        if(i>=m || j>=n) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        else return dp[i][j]= noofpaths(i+1,j,m,n,dp) + noofpaths(i,j+1,m,n,dp);
-    }
+    int f(int i, int j, vector<vector<int>>&dp) {
+    // Base case: reached starting cell
+    if (i == 0 && j == 0) return 1;
+    if(dp[i][j]!=-1) return dp[i][j];
+    int up = 0;
+    int left = 0;
+
+    if (i - 1 >= 0) up = f(i - 1, j,dp);
+    if (j - 1 >= 0) left = f(i, j - 1,dp);
+
+    return dp[i][j] = up + left;
+}
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m,vector<int>(n,-1));
-        return noofpaths(0,0,m,n,dp);
+        return f(m-1,n-1,dp);
     }
 
     // can use m+n-2Cm-1 as m-1 and n-1 moves are to be made to reach final state
