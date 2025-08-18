706315edbe7f0db1f844c5c0c728c525d8fbe154
aksharReddy <127131253+aksharReddy@users.noreply.github.com>
2025-08-18 07:28:47 +0530

    Time: 1 ms (16.20%) | Memory: 9.8 MB (6.36%) - LeetSync

--- 1764-maximum-repeating-substring/maximum-repeating-substring.cpp
+++ 1764-maximum-repeating-substring/maximum-repeating-substring.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    int solve(string &sequence, string &word, int i) {
+    int solve(string &sequence, string &word, int i,vector<int>&dp) {
         // Base case: if remaining length is smaller than word, can't repeat
         if (i + word.size() > sequence.size()) return 0;
 
-        // Check if word starts at index i
+        if(dp[i]!=-1) return dp[i];
         if (sequence.substr(i, word.size()) == word) {
             // If it matches, we can move forward and try extending
-            return 1 + solve(sequence, word, i + word.size());
+            return dp[i]=1 + solve(sequence, word, i + word.size(),dp);
         }
 
         // Otherwise, no repetition starting at i
@@ -17,10 +17,10 @@ public:
     int maxRepeating(string sequence, string word) {
         int n = sequence.size();
         int maxi = 0;
-
+        vector<int>dp(n+1,-1);
         // Try starting at every index
         for (int i = 0; i < n; i++) {
-            maxi = max(maxi, solve(sequence, word, i));
+            maxi = max(maxi, solve(sequence, word, i,dp));
         }
 
         return maxi;
