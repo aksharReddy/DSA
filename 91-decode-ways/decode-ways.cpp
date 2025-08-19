e74487f87729c4185d8969ac0116433f33d557f9
aksharReddy <127131253+aksharReddy@users.noreply.github.com>
2025-08-19 19:33:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (62.90%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,26 +1,44 @@
 class Solution {
 
-    int f(int i,string&s,vector<int>&dp){
-        int n = s.size();
-        if(s[i] == '0') return 0;
-        if(i == n) return 1;
-        if(dp[i]!=-1) return dp[i];
-        //check condition 10<=s[i]s[i+1]<=25 check for 0s if not possbile return 0
-        int takeone = f(i+1,s,dp);
-        int taketwo=0;
-        if(i<n-1){
-            int num = (s[i]-'0')*10 + s[i+1]-'0';
-            if(num>=10 && num<=26){
-                 taketwo = f(i+2,s,dp);
-            }
-        }
-        return dp[i]=takeone + taketwo;
+    // int f(int i,string&s,vector<int>&dp){
+    //     int n = s.size();
+    //     if(s[i] == '0') return 0;
+    //     if(i == n) return 1;
+    //     if(dp[i]!=-1) return dp[i];
+    //     //check condition 10<=s[i]s[i+1]<=25 check for 0s if not possbile return 0
+    //     int takeone = f(i+1,s,dp);
+    //     int taketwo=0;
+    //     if(i<n-1){
+    //         int num = (s[i]-'0')*10 + s[i+1]-'0';
+    //         if(num>=10 && num<=26){
+    //              taketwo = f(i+2,s,dp);
+    //         }
+    //     }
+    //     return dp[i]=takeone + taketwo;
 
-    }
+    // }
 public:
     int numDecodings(string s) {
         int n = s.size();
-        vector<int>dp(n+1,-1);
-        return f(0,s,dp);
+        // vector<int>dp(n+1,-1);
+        // return f(0,s,dp);
+        vector<int>dp(n+1,0);
+
+        dp[n]=1;
+        for(int i=n-1;i>=0;i--){
+            if(s[i]=='0') dp[i] = 0;
+            else{
+                int takeone = dp[i+1];
+                int taketwo=0;
+                if(i<n-1){
+                    int num = (s[i]-'0')*10 + s[i+1]-'0';
+                    if(num>=10 && num<=26){
+                        taketwo= dp[i+2];
+                    }
+                }
+                dp[i] = takeone+taketwo;
+            }
+        }
+        return dp[0];
     }
 };
