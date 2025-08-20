c2bd93b0736653d56f354fdd4f43900853787b6e
aksharReddy <127131253+aksharReddy@users.noreply.github.com>
2025-08-21 02:10:50 +0530

    Time: 25 ms (97.28%) | Memory: 53.7 MB (73.59%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
-    bool palindrome(string&s,int start,int end){
-        while(start<=end){
-            if(s[start]!=s[end]) return false;
-            start++;
-            end--;
-        }
-        return true;
+    vector<vector<int>> memo; // -1 = not calculated, 0 = false, 1 = true
+
+bool palindrome(string &s,int start,int end){
+    if (memo[start][end] != -1) 
+        return memo[start][end]; // return stored result
+
+    int i = start, j = end;
+    while(i <= j){
+        if(s[i] != s[j]) 
+            return memo[start][end] = 0; // false
+        i++;
+        j--;
     }
+    return memo[start][end] = 1; // true
+}
+
 
     void f(int index,string &s,vector<vector<string>>&res,vector<string>&path){
 
@@ -26,6 +34,8 @@ public:
     }
     vector<vector<string>> partition(string s) {
         vector<vector<string>>res;
+        int n = s.size();
+memo.assign(n, vector<int>(n, -1));
         vector<string>path;
         f(0,s,res,path);
         return res;
