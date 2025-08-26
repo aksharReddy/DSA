670829b30b663a43e987097d4afa48607ea5ad11
aksharReddy <127131253+aksharReddy@users.noreply.github.com>
2025-08-26 14:25:14 +0530

    Time: 0 ms (100.00%) | Memory: 22.5 MB (78.39%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,17 +1,27 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        set<int> st;
-        for (auto i : nums) {
-            st.insert(i);
-        }
+        // set<int> st;
+        // for (auto i : nums) {
+        //     st.insert(i);
+        // }
 
-        // Optional: If you want to overwrite nums with unique value
-         int idx = 0;
-        for (int val : st) {
-            nums[idx++] = val;
-        }
+        // // Optional: If you want to overwrite nums with unique value
+        //  int idx = 0;
+        // for (int val : st) {
+        //     nums[idx++] = val;
+        // }
 
-        return st.size();  // Number of unique elements
+        // return st.size();  // Number of unique elements
+        if(nums.empty()) return 0;
+        
+        int i = 0;
+        for(int j = 1; j < nums.size(); j++) {
+            if(nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
     }
 };
