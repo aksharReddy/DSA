65a7bcc56e845b980b6d45635008f5ccff870961
aksharReddy <127131253+aksharReddy@users.noreply.github.com>
2025-12-29 15:55:18 +0530

    Time: 4 ms (47.75%) | Memory: 32.6 MB (80.95%) - LeetSync

---
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int func(vector<int>&nums,int goal){
+        if(goal<0)return 0;
+        int l=0;
+        int r=0;
+        int cnt= 0;
+        int sum=0;
+
+        while(r<nums.size()){
+            sum=sum+nums[r];
+            
+            while(sum>goal){
+                sum=sum - nums[l];
+                l++;
+            }
+            cnt+=r-l+1;
+            r++;
+        }
+        return cnt;
+    }
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        return func(nums,goal) - func(nums,goal-1);
+    }
+};
